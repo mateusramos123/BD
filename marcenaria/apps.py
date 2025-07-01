b91c5e12a4061b0bdcf043705642d2b769dc885a
mateusramos123 <mateus.oliveira.ifc@gmail.com>
2025-07-01 16:42:34 -0300

    Commit Incial

--- marcenaria/apps.py
+++ marcenaria/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MercenariaConfig(AppConfig):
+class MarcenariaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mercenaria'
+    name = 'marcenaria'
