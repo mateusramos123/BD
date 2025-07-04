0fc48572bab8bb2cf6e80eacd34be6497da765d6
mateusramos123 <mateus.oliveira.ifc@gmail.com>
2025-07-04 15:40:41 -0300

    FEAT: Routers Feitos #5

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from marcenaria.views import ClienteViewSet, PedidoViewSet, ProdutoViewSet, ItemPedidoViewSet, MateriaPrimaViewSet, ProdutoHasMateriaPrimaViewSet, FuncionarioViewSet, OrdemProducaoViewSet
+
+router = DefaultRouter()
+router.register(r'Clientes', ClienteViewSet)
+router.register(r'Pedidos', PedidoViewSet)
+router.register(r'Produtos', ProdutoViewSet)
+router.register(r'Itens_Pedidos', ItemPedidoViewSet)
+router.register(r'Materias_Primas', MateriaPrimaViewSet)
+router.register(r'Produtos_Has_Materias_Primas', ProdutoHasMateriaPrimaViewSet)
+router.register(r'Funcionarios', FuncionarioViewSet)
+router.register(r'Ordem_Producoes', OrdemProducaoViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
